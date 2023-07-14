c3ae35962db392a24ca0435d9d9d86448d0937a1
DominikSchle <dominik.schleissing@googlemail.com>
2023-07-14 03:01:57 +0200

    Projektdateien hinzufügen.

---
+++ StudentenNC/Program.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentenNC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Aufgabe: Erstelle ein Programm mit einer Liste von Studenten
+            // Verwende für den Stundenten eine Klasse
+            // Jeder Student hat einen Notendurchschnitt
+            // Berechne dann den durschnittlichen Notendurchschnitt aller Studenten mit einer Schleife
+
+            bool programm = true;
+            students dieStudenten = new students();
+
+            conversationLine("Was möchten Sie tun?");
+            spaceLine();
+
+            while (programm == true)
+            {
+                bool addStudent = true;
+                bool repeat = true;
+                conversationLine("(1) Schüler Hinzufügen");
+                conversationLine("(2) Gesammtdurchschnitt berechnen");
+                conversationLine("(3) Beenden");
+                spaceLine();
+                conversationAsk("Bitte geben Sie ihre Eingabe an: ");
+                string getAnswer = Console.ReadLine();
+                spaceLine();
+
+                if (getAnswer == "1")
+                {
+
+                    while (addStudent == true)
+                    {
+                        conversationLine("Sie 'haben Schüler Hinzufügen' gewählt!");
+                        conversationAsk("War diese Eingabe korrekt? (ja/nein): ");
+                        string getAnswer2 = Console.ReadLine().ToLower();
+                        spaceLine();
+
+                        if (getAnswer2 == "ja")
+                        {
+
+                            conversationLine("Sie haben mit 'ja' geantwortet!");
+                            spaceLine();
+                            while (repeat == true)
+                            {
+                                conversationAsk("Bitte geben Sie den Namen des Schülers ein: ");
+                                string getName = Console.ReadLine();
+                                conversationAsk("Bitte geben Sie nun den Notendurchschnitt an: ");
+                                float getNC = float.Parse(Console.ReadLine());
+                                spaceLine();
+                                conversationLine("Bitte überprüfen Sie ihre Eingaben:");
+                                conversationLine("Name: " + getName);
+                                conversationLine("Notendurchschnitt: " + getNC);
+                                spaceLine();
+                                conversationAsk("War die Eingabe korrekt? (ja/nein/beenden): ");
+                                string getAnswer3 = Console.ReadLine().ToLower();
+                                spaceLine();
+
+                                if (getAnswer3 == "ja")
+                                {
+                                    dieStudenten.AddStudenten(getName, getNC);
+                                    conversationLine("Schüler" + getName + "wurde Hinzugefügt!");
+                                    conversationAsk("Möchten Sie einen weitern schüler Hinzufügen? (ja/Nein): ");
+                                    string getAnswer4 = Console.ReadLine().ToLower();
+                                    spaceLine();
+
+                                    if (getAnswer4 == "ja")
+                                    {
+                                        conversationLine("Sie haben mit 'ja' geantwortet!");
+                                        spaceLine();
+                                    }
+                                    else if (getAnswer4 == "nein")
+                                    {
+                                        conversationLine("Sie haben mit 'nein' geantwortet. Wir kehren nun zum Menü zurück!");
+                                        spaceLine();
+                                        repeat = false;
+                                        addStudent = false;
+                                    }
+                                    else
+                                    {
+                                        conversationLine("Ihre Eingabe war nicht korrekt. Bitte wiederholen!");
+                                        spaceLine();
+                                    }
+                                }
+                                else if (getAnswer3 == "nein")
+                                {
+                                    conversationLine("Sie haben mit 'nein' geantwortet. Bitte wiederholen Sie ihre eingabe!");
+                                    spaceLine();
+                                }
+                                else if (getAnswer3 == "beenden")
+                                {
+                                    conversationLine("Sie haben mit 'beenden' geantwortet. Wir kehren nun zum Menü zurück!");
+                                    spaceLine();
+                                    addStudent = false;
+                                    repeat = false;
+                                }
+                                else
+                                {
+                                    conversationLine("Ihre Eingabe war nicht korrekt. Bitte wiederholen!");
+                                    spaceLine();
+                                }
+                            }
+                        }
+                        else if (getAnswer2 == "nein")
+                        {
+                            conversationLine("Sie haben mit 'nein' geantwortet. Wir kehren nun zum Menü zurück!");
+                            spaceLine();
+                            addStudent = false;
+                        }
+                        else
+                        {
+                            conversationLine("Ihre Eingabe war nicht korrekt. Bitte wiederholen!");
+                            spaceLine();                            
+                        }
+                    }
+                }
+                else if (getAnswer == "2")
+                {
+                    conversationLine("Sie haben 'Gesammtdurchschnitt berechnen' gewählt!");
+                    conversationAsk("War diese Eingabe korrekt? (ja/nein): ");
+                    string getAnswer4 = Console.ReadLine().ToLower();
+                    spaceLine();
+
+                    if (getAnswer4 == "ja")
+                    {
+                        dieStudenten.ShowNC();
+                        conversationLine("Wir kehren nun zum Menü zurück!");
+                        spaceLine();
+                    }
+                    else if (getAnswer4 == "nein")
+                    {
+                        conversationLine("Sie haben mit 'nein' geantwortet. Wir kehren nun zum Menü zurück!");
+                        spaceLine();
+                    }
+                    else
+                    {
+                        conversationLine("Ihre Eingabe war nicht korrekt. Bitte wiederholen!");
+                        spaceLine();
+                    }
+                }
+                else if (getAnswer == "3")
+                {
+                    conversationLine("Vielen Dank! Bitte beehren Sie uns bald wieder <3");
+                    spaceLine();
+                    conversationLine("Bitte nun beliebige Taste zum Beenden drücken. . .");
+                    Console.ReadKey();
+                    programm = false;
+                }
+            }
+        }
+
+        // Ausgabe reiner Text
+        static void conversationLine(string sentence)
+        {
+            Console.WriteLine(sentence);
+        }
+
+        // Ausgabe Anfrage an den Benutzer
+        static void conversationAsk(string question)
+        {
+            Console.Write(question);
+        }
+
+        // Leere Zeile
+        static void spaceLine()
+        {
+            Console.WriteLine();
+        }
+    }
+}
---
+++ StudentenNC/students.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentenNC
+{
+    internal class students
+    {
+        public List<string> StudentName { get; private set; } = new List<string>();
+        public List<float> Durchschnitt { get; private set; } = new List<float>();
+
+
+        // Anlegen eines neuen Studenten
+        public students() { }
+
+        public void AddStudenten(string studentName, float studentNC)
+        {
+            StudentName.Add(studentName);
+            Durchschnitt.Add(studentNC);
+        }
+
+        public void ShowNC()
+        {
+            Console.WriteLine("Der Notendurchschnitt aller Schüler beträgt: " + Durchschnitt.Average());
+        }        
+    }
+}
